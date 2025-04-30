8b713611aef70cc03ab88e72e8dee05248fd767e
dibaihss <souhibznk@gmail.com>
2025-04-30 12:36:13 +0200

    Refactor project structure: remove Procfile and system.properties, add Dockerfile and render.yml, update pom.xml dependencies, and configure application properties for production.

--- src/main/java/com/strategic/ludo/strategicLudo/StrategicLudoApplication.java
+++ src/main/java/com/strategic/ludo/strategicLudo/StrategicLudoApplication.java
@@ -1,10 +1,12 @@
 package com.strategic.ludo.strategicLudo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.strategic.ludo.strategicLudo", "Controllers", "Services"})
